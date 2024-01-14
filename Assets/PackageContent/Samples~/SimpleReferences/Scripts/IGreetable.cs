6236f7c9b6dceddea8a3540dd74ec5251d44f4cc
Oleksii Popov <a.popov@argentics.io>
2024-01-14 23:54:22 +0100

    add sample to package

---
+++ Assets/PackageContent/Samples~/SimpleReferences/Scripts/IGreetable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InterfaceReferencer.Sample
+{
+    public interface IGreetable
+    {
+        void Greetings();
+    }
+}
